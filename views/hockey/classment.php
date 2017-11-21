e3f1fdc4473a11c8c9b8ac50b52314bda421c1ef
Chevalier-Maxime <chevalier.maxime@outlook.com>
2017-11-21 15:35:51 -0500

    Classement rempli

--- views/hockey/classment.php
+++ views/hockey/classment.php
@@ -14,21 +14,131 @@
 	  <tbody>
 	    <tr>
 	      <th scope="row">1</th>
-	      <td>Mark</td>
-	      <td>Otto</td>
-	      <td>@mdo</td>
+	      <td>Tampa Bay</td>
+	      <td>20</td>
+	      <td>15</td>
+          <td>3</td>
+          <td>2</td>
 	    </tr>
 	    <tr>
 	      <th scope="row">2</th>
-	      <td>Jacob</td>
-	      <td>Thornton</td>
-	      <td>@fat</td>
+	      <td>Tonronto</td>
+	      <td>22</td>
+	      <td>14</td>
+          <td>8</td>
+          <td>0</td>
 	    </tr>
 	    <tr>
 	      <th scope="row">3</th>
-	      <td>Larry</td>
-	      <td>the Bird</td>
-	      <td>@twitter</td>
+	      <td>New Jersey</td>
+	      <td>20</td>
+	      <td>12</td>
+          <td>5</td>
+          <td>3</td>
+	    </tr>
+          <tr>
+	      <th scope="row">4</th>
+	      <td>Columbus</td>
+	      <td>21</td>
+	      <td>13</td>
+          <td>7</td>
+          <td>1</td>
+	    </tr>
+          <tr>
+	      <th scope="row">5</th>
+	      <td>Pittsburgh</td>
+	      <td>22</td>
+	      <td>11</td>
+          <td>8</td>
+          <td>3</td>
+	    </tr>
+          <tr>
+	      <th scope="row">6</th>
+	      <td>Islanders de NY</td>
+	      <td>20</td>
+	      <td>11</td>
+          <td>7</td>
+          <td>2</td>
+	    </tr>
+          <tr>
+	      <th scope="row">7</th>
+	      <td>Detroit</td>
+	      <td>21</td>
+	      <td>10</td>
+          <td>8</td>
+          <td>3</td>
+	    </tr>
+          <tr>
+	      <th scope="row">8</th>
+	      <td>Washington</td>
+	      <td>22</td>
+	      <td>11</td>
+          <td>10</td>
+          <td>1</td>
+	    </tr>
+          <tr>
+	      <th scope="row">9</th>
+	      <td>Caroline</td>
+	      <td>19</td>
+	      <td>9</td>
+          <td>6</td>
+          <td>4</td>
+	    </tr>
+          <tr>
+	      <th scope="row">10</th>
+	      <td>Ottawa</td>
+	      <td>19</td>
+	      <td>8</td>
+          <td>5</td>
+          <td>6</td>
+	    </tr>
+          <tr>
+	      <th scope="row">11</th>
+	      <td>Rangers de NY</td>
+	      <td>21</td>
+	      <td>10</td>
+          <td>9</td>
+          <td>2</td>
+	    </tr>
+          <tr>
+	      <th scope="row">12</th>
+	      <td>Boston</td>
+	      <td>19</td>
+	      <td>8</td>
+          <td>7</td>
+          <td>4</td>
+	    </tr>
+          <tr>
+	      <th scope="row">13</th>
+	      <td>Philadelphie</td>
+	      <td>20</td>
+	      <td>8</td>
+          <td>8</td>
+          <td>4</td>
+	    </tr>
+          <tr>
+	      <th scope="row">14</th>
+	      <td>Montréal</td>
+	      <td>21</td>
+	      <td>8</td>
+          <td>11</td>
+          <td>2</td>
+	    </tr>
+          <tr>
+	      <th scope="row">15</th>
+	      <td>Floride</td>
+	      <td>20</td>
+	      <td>7</td>
+          <td>11</td>
+          <td>2</td>
+	    </tr>
+          <tr>
+	      <th scope="row">16</th>
+	      <td>Buffalo</td>
+	      <td>21</td>
+	      <td>5</td>
+          <td>12</td>
+          <td>4</td>
 	    </tr>
 	  </tbody>
 	</table>
@@ -50,21 +160,123 @@
 	  <tbody>
 	    <tr>
 	      <th scope="row">1</th>
-	      <td>Mark</td>
-	      <td>Otto</td>
-	      <td>@mdo</td>
+	      <td>St. Louis</td>
+	      <td>21</td>
+	      <td>15</td>
+          <td>5</td>
+          <td>1</td>
 	    </tr>
-	    <tr>
+          <tr>
 	      <th scope="row">2</th>
-	      <td>Jacob</td>
-	      <td>Thornton</td>
-	      <td>@fat</td>
+	      <td>Winnipeg</td>
+	      <td>20</td>
+	      <td>12</td>
+          <td>5</td>
+          <td>3</td>
 	    </tr>
-	    <tr>
+          <tr>
 	      <th scope="row">3</th>
-	      <td>Larry</td>
-	      <td>the Bird</td>
-	      <td>@twitter</td>
+	      <td>Nashville</td>
+	      <td>22</td>
+	      <td>12</td>
+          <td>6</td>
+          <td>2</td>
+	    </tr>
+          <tr>
+	      <th scope="row">4</th>
+	      <td>Los Angeles</td>
+	      <td>21</td>
+	      <td>12</td>
+          <td>7</td>
+          <td>2</td>
+	    </tr>
+          <tr>
+	      <th scope="row">5</th>
+	      <td>Vegas</td>
+	      <td>19</td>
+	      <td>12</td>
+          <td>6</td>
+          <td>1</td>
+	    </tr>
+          <tr>
+	      <th scope="row">6</th>
+	      <td>Calgary</td>
+	      <td>20</td>
+	      <td>12</td>
+          <td>8</td>
+          <td>0</td>
+	    </tr>
+          <tr>
+	      <th scope="row">7</th>
+	      <td>Anaheim</td>
+	      <td>20</td>
+	      <td>10</td>
+          <td>7</td>
+          <td>3</td>
+	    </tr>
+          <tr>
+	      <th scope="row">8</th>
+	      <td>Chicago</td>
+	      <td>20</td>
+	      <td>10</td>
+          <td>8</td>
+          <td>2</td>
+	    </tr>
+          <tr>
+	      <th scope="row">9</th>
+	      <td>San Jose</td>
+	      <td>19</td>
+	      <td>10</td>
+          <td>8</td>
+          <td>1</td>
+	    </tr>
+          <tr>
+	      <th scope="row">10</th>
+	      <td>Colorado</td>
+	      <td>19</td>
+	      <td>10</td>
+          <td>8</td>
+          <td>1</td>
+	    </tr>
+          <tr>
+	      <th scope="row">11</th>
+	      <td>Dallas</td>
+	      <td>20</td>
+	      <td>10</td>
+          <td>9</td>
+          <td>1</td>
+	    </tr>
+          <tr>
+	      <th scope="row">12</th>
+	      <td>Minnesota</td>
+	      <td>20</td>
+	      <td>9</td>
+          <td>8</td>
+          <td>3</td>
+	    </tr>
+          <tr>
+	      <th scope="row">13</th>
+	      <td>Vancouver</td>
+	      <td>20</td>
+	      <td>9</td>
+          <td>8</td>
+          <td>3</td>
+	    </tr>
+          <tr>
+	      <th scope="row">14</th>
+	      <td>Edmonton</td>
+	      <td>20</td>
+	      <td>7</td>
+          <td>11</td>
+          <td>2</td>
+	    </tr>
+          <tr>
+	      <th scope="row">15</th>
+	      <td>Arizona</td>
+	      <td>23</td>
+	      <td>5</td>
+          <td>15</td>
+          <td>3</td>
 	    </tr>
 	  </tbody>
 	</table>
